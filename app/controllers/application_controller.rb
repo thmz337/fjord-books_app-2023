99b24b8a367e4177a89f23f10b353534e4751bc7
thmz337 <th994209@gmail.com>
2024-05-28 22:02:37 +0900

    ユーザー一覧とユーザー詳細画面の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
   before_action :authenticate_user!
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :self_intro])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address self_intro])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :books
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   root 'books#index'
 end
