c2ab5cab6fdf5f457202c63355d788133dd171b5
thmz337 <th994209@gmail.com>
2024-06-08 21:16:29 +0900

    プロフィール画像のアップデート時に不要なファイルと設定を削除

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Users::RegistrationsController < Devise::RegistrationsController
-  def update
-    super
-    profile_picture = account_update_params[:profile_picture]
-    resource.profile_picture.attach(profile_picture) unless profile_picture.nil?
-  end
-end
