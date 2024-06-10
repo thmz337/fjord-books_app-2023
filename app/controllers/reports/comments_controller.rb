cf01e6c5515124a030af797a6cfe7df4a39e166d
thmz337 <th994209@gmail.com>
2024-06-10 22:29:34 +0900

    日報と本のコメントコントローラを分離

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
-class CommentsController < ApplicationController
+class Reports::CommentsController < ApplicationController
   before_action :set_commentable, only: %i[create destroy]
   before_action :set_comment, only: %i[destroy]
 
   # POST #{commenatble}/:commentable_id/comments or #{commentable}/:commentable_id/comments.json
   def create
-    @comment = @commentable.comments.create(comment_params)
-    @comment.user = current_user
+    comment = @commentable.comments.create(comment_params)
+    comment.user = current_user
 
-    commentable_class_name = @commentable.class.name.downcase
-    # for show method of commentable
-    instance_variable_set("@#{commentable_class_name}".to_sym, @commentable)
-
-    if @comment.save
+    if comment.save
       redirect_to report_url(@commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
       render 'reports/show', status: :unprocessable_entity
@@ -24,26 +20,17 @@ class CommentsController < ApplicationController
   def destroy
     return unless @comment.user_id == current_user.id
 
-    @comment.destroy
+    @comment.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to report_url(@commentable), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
-      format.json { head :no_content }
-    end
+    redirect_to report_url(@commentable), notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_commentable
-    url_path_elements = request.path.split(%r{/})[1..]
-
-    case url_path_elements
-    in ['reports', *]
-      @commentable = Report.find(params['report_id'])
-    in ['books', *]
-      @commentable = Book.find(params['book_id'])
-    end
+    commentable_id = params[:report_id]
+    @commentable = Report.find(commentable_id)
   end
 
   def set_comment
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -25,14 +25,10 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.create(report_params)
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-        format.json { render :show, status: :created, location: @report }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
