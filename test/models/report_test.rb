d1bfe4b51d1991e4347bb3c5a427f83f31db2190
thmz337 <th994209@gmail.com>
2024-06-12 00:22:41 +0900

    Reportモデルのテストにおいて、日付の比較をDateクラスで比較するように変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -10,11 +10,11 @@ class ReportTest < ActiveSupport::TestCase
 
   test 'user can edit report' do
     report = reports(:first)
-    assert_not report.editable?(users(:alice))
+    assert report.editable?(users(:alice))
   end
 
   test 'Get the date when the daily report was created' do
     report = reports(:second)
-    assert 'Tue, 11 Jun 2024', report.created_on
+    assert_equal Date.parse('Tue, 11 Jun 2024'), report.created_on
   end
 end
